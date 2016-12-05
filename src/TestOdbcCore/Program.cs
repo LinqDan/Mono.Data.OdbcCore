68f0741d492718f348951684a356330348c0c025
LinqDan <LinqDan2000@gmail.com>
2016-12-04 21:44:11 -0500

    First commit

---
+++ src/TestOdbcCore/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+
+namespace TestOdbcCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Start.");
+
+            Console.WriteLine("create OdbcConnection object...");
+            using (var connection = new OdbcConnection("DSN=SQL16"))
+            {
+                Console.WriteLine("Open connection...");
+                connection.Open();
+
+                Console.WriteLine("Create command...");
+                using (var command = connection.CreateCommand())
+                {
+
+                    command.CommandText = "select TABLE_NAME from INFORMATION_SCHEMA.TABLES";
+
+                    Console.WriteLine("Execute reader...");
+                    using (var reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("Tables:");
+
+                        while (reader.Read())
+                        {
+                            string tableName = reader.GetString(reader.GetOrdinal("TABLE_NAME"));
+                            Console.WriteLine(tableName);
+                        };
+                    }
+                }
+
+                Console.WriteLine("Done.");
+
+            }
+        }
+    }
+}
